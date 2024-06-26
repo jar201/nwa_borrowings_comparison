1664d1d33275816a8e76f3ac2fda346cc3bcfbfd
jar201 <145118396+jar201@users.noreply.github.com>
2024-06-26 11:01:42 +0300

    lemma-filter-update

--- aramaic/scripts/db_lemma_recovery.py
+++ aramaic/scripts/db_lemma_recovery.py
@@ -14,13 +14,14 @@ etymology = []
 
 for line in lines:
     m = re.search(r"\<(.*)\>\s\-\-\s(\d|)", line)
-    g = m.groups()
-    # forming data from matched groups
-    c = g[0].strip()
-    t = txt_to_digit(g[1].strip())
-    # adding words to our parallel data
-    new_words.append(c)
-    etymology.append(t)
+    if m != None:
+        g = m.groups()
+        # forming data from matched groups
+        c = g[0].strip()
+        t = txt_to_digit(g[1].strip())
+        # adding words to our parallel data
+        new_words.append(c)
+        etymology.append(t)
 
 new_data = pd.DataFrame({"word": new_words, "etymology": etymology})
 new_data.to_csv("aramaic/lemma_etymology.csv")
